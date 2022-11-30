0bacd20d900cb95158644033d1308a7bb2957397
tison.yin <yinqizhen@aliyun.com>
2022-11-30 15:05:23 +0800

    update

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,6 +1,7 @@
 package metrics
 
 // Counter is metrics counter.
+// Counter 只增不减的计数器， 代表指标累计值
 type Counter interface {
 	With(lvs ...string) Counter
 	Inc()
@@ -8,6 +9,7 @@ type Counter interface {
 }
 
 // Gauge is metrics gauge.
+// Gauge 可增可减的仪表盘， 反应系统的当前状态，
 type Gauge interface {
 	With(lvs ...string) Gauge
 	Set(value float64)
@@ -16,6 +18,7 @@ type Gauge interface {
 }
 
 // Observer is metrics observer.
+// Observer 指标统计数据
 type Observer interface {
 	With(lvs ...string) Observer
 	Observe(float64)
